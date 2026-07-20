57c735efdcd9e2753f750219a041d14432df5e45
Igor Vučko <83359466+igorvucko@users.noreply.github.com>
2026-07-20 18:15:00 +0200

    vj

---
+++ 02_HTML_CSS/vjezba12/JS_03/vjezba05/skripta.js
@@ -0,0 +1,9 @@
+//if grananje
+//boolean tip podataka 
+
+
+const uvjet = false;
+//minimalna if sintaksa
+if(uvjet) {// u if granu se ulazi ako je uvjet true 
+    console.log("Uvjet je zadovoljen");
+}
