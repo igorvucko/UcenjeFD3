49835285b8c923d987007b66fbb1f4c63c7c6eca
Igor Vučko <83359466+igorvucko@users.noreply.github.com>
2026-07-27 17:06:35 +0200

    vjezba

---
+++ 02_HTML_CSS/vjezba12/JS_03/vjezba07/skripta.js
@@ -0,0 +1,19 @@
+const t1 = 4;
+
+const prazanNiz = [];
+
+console.log('prazan niz',prazanNiz);
+
+console.log('duzina niza', prazanNiz.length);
+
+const temp= [-4,1.8,9,25,14,0,3.14];
+
+console.log('temp', temp.length);
+
+console.log('zadnji element niza', temp[temp.length-1]);
+
+const mjesta = 
+
+['Zagreb', 'Split', 'Rijeka', 'Osijek', 'Pula'];
+
+console.log('prvi element niza', mjesta[0]);
